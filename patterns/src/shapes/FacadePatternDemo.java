f534cdc4c58ef16174fd385c074610ef74621080
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 11:11:21 +0530

    Create FacadePatternDemo.java

---
+++ patterns/src/shapes/FacadePatternDemo.java
@@ -0,0 +1,13 @@
+package shapes;
+
+public class FacadePatternDemo {
+
+	public static void main(String[] args) {
+		ShapeMaker shape = new ShapeMaker();
+		shape.drawCircle();
+		shape.drawRectangle();
+		shape.drawSquare();
+		shape.drawTriangle();
+	}
+
+}
