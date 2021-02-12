725fc729828541180795a4a4e3323b75e17e1293
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:25:47 +0530

    Create FurnitureOrder.java

---
+++ design-final/src/retail/FurnitureOrder.java
@@ -0,0 +1,10 @@
+package retail;
+
+public class FurnitureOrder extends Order {
+
+	@Override
+	public Product processOrder(Channel channel, ProductType productType) {
+		return new Furniture(channel);
+	}
+
+}
