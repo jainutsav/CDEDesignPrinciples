dc022fe275cccb6bab084aad6b71a5984c14602c
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 11:06:18 +0530

    Create State.java

---
+++ patterns/src/message/State.java
@@ -0,0 +1,18 @@
+package message;
+
+public class State {
+	private String state;
+
+	public State(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
