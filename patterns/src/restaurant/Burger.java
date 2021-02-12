7e863d32581828a96eec0916715375ead6908185
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 11:07:52 +0530

    Create Burger.java

---
+++ patterns/src/restaurant/Burger.java
@@ -0,0 +1,17 @@
+package restaurant;
+
+public abstract class Burger implements Item {
+
+	@Override
+
+	public Packing packing() {
+
+		return new Wrapper();
+
+	}
+
+	@Override
+
+	public abstract float price();
+
+}
