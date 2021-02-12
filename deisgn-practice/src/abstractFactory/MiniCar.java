79e7d8ebcb4c9b223f431283b42aa5437e4e5f17
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:12:02 +0530

    Create MiniCar.java

---
+++ deisgn-practice/src/abstractFactory/MiniCar.java
@@ -0,0 +1,16 @@
+package abstractFactory;
+
+public class MiniCar extends Car {
+
+	MiniCar(Location location) {
+		super(CarType.MINI, location);
+		construct();
+	}
+
+	@Override
+	void construct() {
+		System.out.println("Connecting to mini car");
+
+	}
+
+}
