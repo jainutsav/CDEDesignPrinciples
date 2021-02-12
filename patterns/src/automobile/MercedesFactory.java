9c3786d6b22ea9888ab560dcdf1f920f92a3afac
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:56:48 +0530

    Create MercedesFactory.java

---
+++ patterns/src/automobile/MercedesFactory.java
@@ -0,0 +1,11 @@
+package automobile;
+
+public class MercedesFactory extends Factory {
+	public Headlight makeHeadlight() {
+		return new MercedesHeadlight();
+	}
+
+	public Tire makeTire() {
+		return new MercedesTire();
+	}
+}
