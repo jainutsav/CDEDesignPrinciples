a89567207fd1bcba77d36dbb09bc3ad850ea2e70
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:08:56 +0530

    Create Car.java

---
+++ deisgn-practice/src/abstractFactory/Car.java
@@ -0,0 +1,43 @@
+package abstractFactory;
+enum CarType {
+	MICRO, MINI, LUXURY
+}
+
+enum Location {
+	DEFAULT, USA, INDIA
+}
+
+abstract class Car {
+
+	Car(CarType model, Location location) {
+		this.model = model;
+		this.location = location;
+	}
+
+	abstract void construct();
+
+	CarType model = null;
+	Location location = null;
+
+	public CarType getModel() {
+		return model;
+	}
+
+	public void setModel(CarType model) {
+		this.model = model;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [model=" + model + ", location=" + location + "]";
+	}
+
+}
