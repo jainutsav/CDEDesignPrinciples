cc0297821ebe7765dc0e0133e0fb00b29f2c2c48
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:25:31 +0530

    Create Furniture.java

---
+++ design-final/src/retail/Furniture.java
@@ -0,0 +1,16 @@
+package retail;
+
+public class Furniture extends Product {
+
+	public Furniture(Channel channel) {
+		super(channel, ProductType.FURNITURE);
+		this.processOrder();
+	}
+
+	@Override
+	public void processOrder() {
+		System.out.println("Processing furniture order.");
+
+	}
+
+}
