1990c74c15809cd8fb7260dfcc377d23aff3068e
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:54:51 +0530

    Create AudiFactory.java

---
+++ patterns/src/automobile/AudiFactory.java
@@ -0,0 +1,11 @@
+package automobile;
+
+public class AudiFactory extends Factory {
+	public Headlight makeHeadlight() {
+		return new AudiHeadlight();
+	}
+
+	public Tire makeTire() {
+		return new AudiTire();
+	}
+}
