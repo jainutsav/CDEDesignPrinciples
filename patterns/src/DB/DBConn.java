9837c8c4e64c3aee5a769b193ea278281b042ac8
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:54:08 +0530

    Create DBConn.java

---
+++ patterns/src/DB/DBConn.java
@@ -0,0 +1,14 @@
+package DB;
+
+public class DBConn {
+	private static DBConn instance = new DBConn();
+
+	private DBConn() {
+	}
+
+	public static DBConn getInstance() {
+		return instance;
+
+	}
+
+}
