c89aba104bd1f661cfa1341ddf0140a6190ca73c
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:59:22 +0530

    Create TestCases.java

---
+++ patterns/src/cars/TestCases.java
@@ -0,0 +1,21 @@
+package cars;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCases {
+	@Test
+	public void whenConvertingMPHToKMPH_thenSuccessfullyConverted() {
+		Movable bugattiVeyron = new BugattiVeyron();
+		MovableAdapter bugattiVeyronAdapter = new MovableAdapterImpl(bugattiVeyron);
+		assertEquals(bugattiVeyronAdapter.getSpeed(), 431.30312, 0.00001);
+	}
+	
+	@Test
+	public void whenConvertingUSDToEuro_thenSuccessfullyConverted() {
+		Movable bugattiVeyron = new BugattiVeyron();
+		MovableAdapter bugattiVeyronAdapter = new MovableAdapterImpl(bugattiVeyron);
+		assertEquals(bugattiVeyronAdapter.getPrice(), 1310863.5708, 0.00001);
+	}
+}
