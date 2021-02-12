d11c9d8a9309a3d0a9f83baf9853757395dc627c
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:54:24 +0530

    Create Main.java

---
+++ patterns/src/DB/Main.java
@@ -0,0 +1,12 @@
+package DB;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(DBConn.getInstance());
+		System.out.println(DBConn.getInstance());
+
+
+	}
+}
