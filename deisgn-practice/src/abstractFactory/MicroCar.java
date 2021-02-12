c25ee95ace52b63fd2ad5f90a6cb00d7246a36ec
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 10:11:12 +0530

    Create MicroCar.java

---
+++ deisgn-practice/src/abstractFactory/MicroCar.java
@@ -0,0 +1,16 @@
+package abstractFactory;
+
+public class MicroCar extends Car {
+
+	MicroCar(Location location) {
+		super(CarType.MICRO, location);
+		construct();
+	}
+
+	@Override
+	void construct() {
+		System.out.println("Connecting to micro car");
+
+	}
+
+}
