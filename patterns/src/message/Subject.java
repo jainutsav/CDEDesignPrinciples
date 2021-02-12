99e30e7520cc6d6c1d805edb8c0e88971861bbb7
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-02-12 11:06:32 +0530

    Create Subject.java

---
+++ patterns/src/message/Subject.java
@@ -0,0 +1,13 @@
+package message;
+
+public interface Subject {
+
+	public void attach(Observer o);
+
+	public void detach(Observer o);
+
+	public void notifyUpdate(Message m);
+
+	void notifyStateUpdate(State s);
+
+}
